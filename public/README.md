61cae648b6cf51770beff83b82b22a04b42fcbd6
Luciano Ricardo <lucianoricardo737@gmail.com>
2021-10-23 13:41:15 -0300

    fix bugs

---
+++ public/README.md
@@ -0,0 +1,2 @@
+# CostillaGame
+Personal version for snake game. But eat ribs.
---
+++ public/states/dataUser.js
@@ -0,0 +1,10 @@
+// import { db } from "../auth/firebase.js"
+
+
+export let data = {
+    userId:'',
+    email:'',
+    topScore:'',
+    attempts:'',
+
+}
